5489382ad0f9229029eaeefaedb31347815294f4
Bhavya248 <117252069+Bhavya248@users.noreply.github.com>
2022-11-08 21:50:39 -0800

    Create Creating Pyramid of Odd values.py

---
+++ Creating Pyramid of Odd values.py
@@ -0,0 +1,20 @@
+n = int(input('provide levels for pattern'))
+writeable_spaces = 2*n + 1
+star_levels=[]
+for i in range(0,n):
+    star_levels.append(1+2*i)
+print(star_levels)    
+for i in range(0,n):
+        whites = n-i-1
+        pattern = []
+        for p in range(whites,0,-1):
+            pattern.append(" ")
+            le = [" " for i in range(1+2*(i+1))]
+        pattern.append("*"*star_levels[i])
+        for r in range(whites,0,-1):
+            pattern.append(" ") 
+        for s in pattern:
+            print(s,end="")
+        print("",end="\n")
+        #print(pattern)
+        pattern=[]    

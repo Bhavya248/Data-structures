f08b92b5e3966f5e82725537d954f9ffd2df8498
Bhavya248 <shah.bhavya248@gmail.com>
2022-12-08 12:19:00 +0000

    #

--- create a full diamond.py
+++ create a full diamond.py
@@ -1,11 +1,20 @@
-n = int(input("Enter a number for the patter -->  "))
+# full diamond pattern
+n = int(input("Enter a number for the pattern -->  "))
 fields = 2*n+1
-pattern = [" "for i in range(n)]
+pattern = [" "for i in range(fields)]
 count = 0 
 temp = n
 if n % 2 == 0 :
-        count = n / 2
-else:         
-while temp > 0:
-    
-    temp = temp - 1
+        count = int(n / 2)
+else: 
+    count = int((n + 1) / 2)        
+for i in range(count,0,-1):
+    inex_a = int((fields+1)/2)
+    inex_b = int((fields+1)/2)
+    while temp > 0 :
+        pattern[inex_a-1]="*"
+        pattern[inex_b-1]="*"
+        print(" ".join(pattern))
+        inex_a = inex_a + 1
+        inex_b = inex_b - 1
+        temp = temp - 1    

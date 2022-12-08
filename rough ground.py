5bc4ebeb3d0b9af8724e6526e56595641c52238f
Bhavya248 <shah.bhavya248@gmail.com>
2022-12-08 13:57:15 +0000

    #

---
+++ rough ground.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number  "))
+big_list = []
+num = 2
+for i in range(1,n+1):
+    big_list.append([])
+    for p in range(1,n+1):
+        if len(big_list) > 1:
+            big_list[i].append(num*p)
+            
+            

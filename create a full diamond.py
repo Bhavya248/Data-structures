89b93055785a93d83535ce33b3f0a551f77737b4
Bhavya248 <shah.bhavya248@gmail.com>
2022-12-08 12:20:41 +0000

    #

--- create a full diamond.py
+++ create a full diamond.py
@@ -8,13 +8,13 @@ if n % 2 == 0 :
         count = int(n / 2)
 else: 
     count = int((n + 1) / 2)        
-for i in range(count,0,-1):
-    inex_a = int((fields+1)/2)
-    inex_b = int((fields+1)/2)
-    while temp > 0 :
-        pattern[inex_a-1]="*"
-        pattern[inex_b-1]="*"
-        print(" ".join(pattern))
-        inex_a = inex_a + 1
-        inex_b = inex_b - 1
-        temp = temp - 1    
+#for i in range(count,0,-1):
+inex_a = int((fields+1)/2)
+inex_b = int((fields+1)/2)
+while temp > 0 :
+    pattern[inex_a-1]="*"
+    pattern[inex_b-1]="*"
+    print(" ".join(pattern))
+    inex_a = inex_a + 1
+    inex_b = inex_b - 1
+    temp = temp - 1    

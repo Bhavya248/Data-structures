337c37aa049ad420a785c3cfc09610d1e637b79e
Bhavya248 <shah.bhavya248@gmail.com>
2022-12-09 04:25:18 +0000

    #

--- rough ground.py
+++ rough ground.py
@@ -1,17 +1,12 @@
 n = int(input("Enter a number  "))
 lista = [[]for i in range(n)]
 num = 2
-print(lista)
 for i in range(len(lista)) :
     count = n - i
+    temp = 0 
     while count >= 1 :
-        lista[i].append(num + (2*()))
+        lista[i].append(num + (2*(temp)))
         count = count - 1
+        temp = temp + 1
     num = num + 1  
-'''big_list = reversed(big_list)
-for i in range(n):
-    pattern = [" "for i in range(2*n + 1)]
-
-
-             /'''
 print(lista)

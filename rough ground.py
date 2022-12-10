ad3de4857568822c37528f6dcf5ccb671fce3b0b
Bhavya248 <shah.bhavya248@gmail.com>
2022-12-10 05:27:31 +0000

    #

--- rough ground.py
+++ rough ground.py
@@ -9,10 +9,9 @@ for i in range(len(lista)) :
         count = count - 1
         temp = temp + 1
     num = num + 1  
-print(lista)
 pattern = [" " for i in range(2*n+1)]
-for i in range(len(lista)):
+for i in range(len(lista)-1,-1,-1):
     for m in lista[i]:
-        print(m)
-            pattern[m] = "*"
+        pattern[m] = "*"
     print(" ".join(pattern))
+    pattern = [" " for i in range(2*n+1)]

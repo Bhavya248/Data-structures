4e429499092b0924005ede66008251b5eddc10b4
Bhavya Shah <117252069+Bhavya248@users.noreply.github.com>
2022-12-11 04:51:33 +0000

    #

--- Diamond Tree.py
+++ Diamond Tree.py
@@ -10,11 +10,14 @@ for i in range(n):
         diamond_tree[i].append(str(num))
         num = num+1
     print(" ".join(diamond_tree[i]))
-
 # The next step will now shorten the tree by summing up values
 diamond_tree = diamond_tree[-1]
-base_groups = int(horiz_length[-1]/2)
-short = [[] for i in range(base_groups)]
-for i in range((base_groups)):
-    short[i].append(diamond_tree[2*i])
-    short[i].append(diamond_tree[2*i+1])
+temp = n
+while temp > 0:
+    short = []
+    for i in range(int(len(diamond_tree)/2)):
+        short.append(str(int(diamond_tree[2*i]) + int(diamond_tree[2*i+1])))
+    print(" ".join(short))
+    diamond_tree = short  
+    temp = temp - 1
+    del short

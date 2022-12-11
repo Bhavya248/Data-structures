08816df203f12abc918bb1fe860b627f12102085
Bhavya Shah <117252069+Bhavya248@users.noreply.github.com>
2022-12-11 04:19:18 +0000

    #

--- Diamond Tree.py
+++ Diamond Tree.py
@@ -5,17 +5,19 @@ for i in range(n):
     horiz_length.append(2**i)
 num = 1
 diamond_tree = [[] for i in range(n)]
-for i in range(n) :
+for i in range(n):
     for p in range(horiz_length[i]):
-        diamond_tree[i].append(num)
-        num=num+1
-    print(" ".join(str(diamond_tree[i])))
+        diamond_tree[i].append(str(num))
+        num = num+1
+    print(" ".join((diamond_tree[i])))
 
 # The next step will now shorten the tree by summing up values
 
 base_groups = horiz_length[-1]/2
 temp = base_groups
-for i in range():
-while temp >= 0 :
+short = []
+for i in range(int(base_groups)):
+    while temp > 0:
 
-    temp = temp - 1
+        temp = temp - 1
+print(short)

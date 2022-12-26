8018ef9b60a34e854a67c9f46dbdf3116f6ef0f8
Bhavya Shah <117252069+Bhavya248@users.noreply.github.com>
2022-12-26 04:24:37 +0000

    #

--- BlockChain Simulator.py
+++ BlockChain Simulator.py
@@ -3,3 +3,6 @@ a block is created every 10 seconds and a independent list of
 transactions that are generated in this time are added to the block 
 and the block details are printed   
 """
+import time
+import random
+import hashlib

ea4d819c0448293da5641253a0735908a58e02fe
Bhavya Shah <117252069+Bhavya248@users.noreply.github.com>
2023-05-31 10:17:22 +0530

    Delete BlockChain Simulator.py

--- BlockChain Simulator.py
+++
@@ -1,8 +0,0 @@
-"""This code is a blockchain simulator , when the code runs
-a block is created every 10 seconds and a independent list of
-transactions that are generated in this time are added to the block 
-and the block details are printed   
-"""
-import time
-import random
-import hashlib

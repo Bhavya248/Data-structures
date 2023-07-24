379debb92c620583eb578b2f67b7b1f216bfb685
Bhavya Shah <117252069+Bhavya248@users.noreply.github.com>
2023-07-24 15:40:08 +0530

    Update Priority Queues.py

--- Priority Queues.py
+++ Priority Queues.py
@@ -19,7 +19,8 @@ class PriorityQueue:
         if (self.index == -1):
             return -1
         else:
-            for i in range(self.index+1):
+            for i in range(self.index):
+                print(ind,i)
                 if (self.items[ind].priority < self.items[i].priority):
                     ind = i
         return ind
@@ -30,3 +31,22 @@ class PriorityQueue:
             del self.items[ind]
         else:
             print("Priority Queue Is empty")
+    
+    def getElements(self):
+        return [self.items[i].key for i in range(len(self.items))]
+
+PQ = PriorityQueue('New')
+PQ.enqueue(1,10)
+PQ.enqueue(2,8)
+PQ.enqueue(3,9)
+PQ.enqueue(4,1)
+PQ.enqueue(5,5)
+print(PQ.getElements())
+PQ.dequeue()
+print(PQ.getElements())
+PQ.dequeue()
+print(PQ.getElements())
+PQ.dequeue()
+print(PQ.getElements())
+PQ.dequeue()
+print(PQ.getElements())
